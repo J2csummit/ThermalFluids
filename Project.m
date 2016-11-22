672150b158ec563d5b0e87ac7be71e6dbeff1147
J2csummit <J2csummit@mac.com>
2016-11-22 08:35:52 -0500

    Obtained Values for Ts and changed method of calculating Ts

---
+++ Project.m
@@ -0,0 +1,3 @@
+clc, clear all
+panel = ThermodynamicPanel(0.04976, 0.1, 5, 342.7317, 329.1463);
+Ts = caluculateTs(panel, 208.3, 296, 350);
--- interpolate.m
+++ interpolate.m
@@ -10,16 +10,18 @@ a1 = 0; a2 = 0;
 b1 = 0; b2 = 0;
 
 startColumn = table(:,columnStart);
-for i=1:length(startColumn)
+i = 1;
+
+while i < length(startColumn)
     a2 = startColumn(i);
     if a2 > startValue && i > 1
         a1 = startColumn(i-1);
         b1 = table(i-1, columnDesired);
         b2 = table(i, columnDesired);
+        i = length(startColumn) + 1;
     end
+    i = i + 1;
 end
-
-assert(a1 == 0 && b1 == 0 && b2 == 0)
 
 value = ((startValue - a1)/(a2 - a1))*(b2 - b1) + b1;
 
